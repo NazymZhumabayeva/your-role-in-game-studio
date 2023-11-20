b7816f1459004ad7c13e4f5b051de306b8b2ad1b
NazymZhumabayeva <zhumabayevanazym@gmail.com>
2023-11-20 15:40:02 +0600

    added "theme" and fixed the bug

--- new.js
+++ new.js
@@ -15,7 +15,7 @@ function updateQuestionStyles() {
         if (element.textContent.includes("?") && !element.classList.contains('question')) {
             element.classList.add('question');
         }
-        else if (element.textContent.includes("producer") || element.textContent.includes("artist") || element.textContent.includes("developer") || element.textContent.includes("designer") || element.textContent.includes("әзірлеуші") || element.textContent.includes("суретші") || element.textContent.includes("дизайнер") || element.textContent.includes("разработчик") || element.textContent.includes("художник") || element.textContent.includes("продюсер"))
+        else if (element.textContent.includes("Universal!") || element.textContent.includes("Жан-жақты адамсыз!") || element.textContent.includes("Универсал!") || element.textContent.includes("producer") || element.textContent.includes("artist") || element.textContent.includes("developer") || element.textContent.includes("designer") || element.textContent.includes("әзірлеуші") || element.textContent.includes("суретші") || element.textContent.includes("дизайнер") || element.textContent.includes("разработчик") || element.textContent.includes("художник") || element.textContent.includes("продюсер"))
         {
             element.classList.add('finalresult');
         }
